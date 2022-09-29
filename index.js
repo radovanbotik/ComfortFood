8aebd521470dfb6181faef22932a0b00eda09e2f
radovanbotik <radovanbotik@gmail.com>
2022-09-29 18:12:25 +0200

    ''

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { sliderComponent } from "./sliderComponent.js";
 import { revealLinks } from "./navComponent.js";
+import { sliderCards } from "./sliderCards.js";
 
 const init = () => {
   sliderComponent();
   revealLinks();
+  sliderCards();
 };
 
 window.addEventListener("DOMContentLoaded", init);
---
+++ sliderCards.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector("#slider-reviews");
+const slides = [...document.querySelectorAll("[data-id='card-review']")];
+const slideWidth = slides[0].getBoundingClientRect().width;
+const btnPrev = document.querySelector("#reviews-button-prev");
+const btnNext = document.querySelector("#reviews-button-next");
+
+let counter = 0;
+const spreadSlides = () => {
+  slides.forEach((slide, index) => {
+    slide.style.left = `${index * slideWidth}px`;
+  });
+};
+
+const translateSlides = () => {
+  slides.forEach(slide => {
+    slide.style.transform = `translateX(-${100 * counter}%)`;
+  });
+};
+
+const sliderCards = () => {
+  spreadSlides();
+
+  btnPrev.addEventListener("click", () => {
+    counter--;
+    if (counter < 0) {
+      counter = slides.length - 1;
+    }
+    translateSlides();
+  });
+
+  btnNext.addEventListener("click", () => {
+    counter++;
+    if (counter > slides.length - 1) {
+      counter = 0;
+    }
+    translateSlides();
+  });
+};
+
+export { sliderCards };
